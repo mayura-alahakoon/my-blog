075e0ad44f6660d660c81b4a2b57916489be97fa
mayura-alahakoon <mayuraalahakoon@gmail.com>
2023-02-25 21:16:04 +0530

    init

---
+++ .next/server/chunks/845.js
@@ -0,0 +1,44 @@
+"use strict";
+exports.id = 845;
+exports.ids = [845];
+exports.modules = {
+
+/***/ 7845:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var styled_jsx_style__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(9816);
+/* harmony import */ var styled_jsx_style__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(styled_jsx_style__WEBPACK_IMPORTED_MODULE_1__);
+
+
+const YEAR = new Date().getFullYear();
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
+    footer: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("small", {
+        style: {
+            display: 'block',
+            marginTop: '3rem'
+        },
+        className: "jsx-7c359fde634f3543",
+        children: [
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("time", {
+                className: "jsx-7c359fde634f3543",
+                /*#__PURE__*/ children: YEAR
+            }),
+            " \xa9 Mayura Andrew.",
+            react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((styled_jsx_style__WEBPACK_IMPORTED_MODULE_1___default()), {
+                id: "7c359fde634f3543",
+                children: "a.jsx-7c359fde634f3543{float:right}\n@media screen and (max-width:480px) {article.jsx-7c359fde634f3543{padding-top:2rem;\npadding-bottom:4rem}}"
+            })
+        ]
+    })
+});
+
+
+/***/ })
+
+};
+;

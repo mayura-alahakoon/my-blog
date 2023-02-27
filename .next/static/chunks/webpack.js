b9b319f5705b1d0cd33ae8a7f481206347a1517d
mayura-alahakoon <mayuraalahakoon@gmail.com>
2023-02-27 14:32:15 +0530

    init

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "42e1c309e0189cd4"; }
+/******/ 		__webpack_require__.h = function() { return "83caa96003df89f0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

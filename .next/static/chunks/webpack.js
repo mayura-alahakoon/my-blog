858accf03b5cefd26489eb12717152681ba8a0c3
mayura-alahakoon <mayuraalahakoon@gmail.com>
2023-02-27 14:41:16 +0530

    init

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "83caa96003df89f0"; }
+/******/ 		__webpack_require__.h = function() { return "039ae75e3ce9267d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/posts/my-story.md
+++ pages/posts/my-story.md
@@ -1,7 +1,7 @@
 ---
 title: Why I become a Open-source enthusiast ?
 date: 2023/2/25
-description: This article about the my story
+description: This article is about my journey with open-source software.
 tag: open-source
 author: me
 ---
